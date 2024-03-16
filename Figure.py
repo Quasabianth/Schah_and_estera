84e4db8a5096de6f9e1ac03ce586ff4955e48fba
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-16 21:18:23 +0300

    Update Figure.py

--- Figure.py
+++ Figure.py
@@ -6,7 +6,7 @@ class Figure:
 
 
 class Pawn(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -16,12 +16,12 @@ class Pawn(Figure):
         moves = []
         assert self.point.pos_y != 1
         assert self.point.pos_y != 8
-        if self.color == 0:
+        if self.color == 'w':
             moves += [str(self.point.pos_x + a) + str(self.point.pos_y + 1) for a in [-1, 0, 1]
                       if 1 <= self.point.pos_x + a <= 8]
             if self.point.pos_y == 2:
                 moves.append(str(self.point.pos_x + 1) + str(4))
-        elif self.color == 1:
+        elif self.color == 'b':
             moves = [str(self.point.pos_x + a) + str(self.point.pos_y - 1) for a in [-1, 0, 1]
                      if 1 <= self.point.pos_x + a <= 8]
             if self.point.pos_y == 7:
@@ -33,7 +33,7 @@ class Pawn(Figure):
 
 
 class King(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -50,7 +50,7 @@ class King(Figure):
 
 
 class Knight(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -69,7 +69,7 @@ class Knight(Figure):
 
 
 class Bishop(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -90,7 +90,7 @@ class Bishop(Figure):
 
 
 class Rook(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -109,7 +109,7 @@ class Rook(Figure):
 
 
 class Queen(Figure):
-    def __init__(self, color: int, point: Point):
+    def __init__(self, color: str, point: Point):
         self.color = color
         self.moves = self.move()
         self.point = point
@@ -128,7 +128,6 @@ class Queen(Figure):
         moves += [str(self.point.pos_x + b) + str(self.point.pos_y) for b in range(-8, 9)
                   if 1 <= self.point.pos_x + b <= 8]
         return set(moves)
-    
+
     def delete(self):
         pass
-
