96b42496bbdae2c18c244aaa9a1aadb6d320ab8d
Quasabianth <163635474+Quasabianth@users.noreply.github.com>
2024-03-16 15:07:56 +0300

    Update Figure.py

--- Figure.py
+++ Figure.py
@@ -16,17 +16,13 @@ class Pawn(Figure):
         assert self.point.pos_y != 1
         assert self.point.pos_y != 8
         if self.color == 0:
-            cand = [str(self.point.pos_x + a) + str(self.point.pos_y + 1) for a in [-1, 0, 1]]
-            for tr in cand:
-                if 1 <= int(tr[0]) <= 8:
-                    moves.append(tr)
+            moves += [str(self.point.pos_x + a) + str(self.point.pos_y + 1) for a in [-1, 0, 1]
+                      if 1 <= self.point.pos_x + a <= 8]
             if self.point.pos_y == 2:
                 moves.append(str(self.point.pos_x + 1) + str(4))
         elif self.color == 1:
-            cand = [str(self.point.pos_x + a) + str(self.point.pos_y - 1) for a in [-1, 0, 1]]
-            for tr in cand:
-                if 1 <= int(tr[0]) <= 8:
-                    moves.append(tr)
+            moves = [str(self.point.pos_x + a) + str(self.point.pos_y - 1) for a in [-1, 0, 1]
+                     if 1 <= self.point.pos_x + a <= 8]
             if self.point.pos_y == 7:
                 moves.append(str(self.point.pos_x + 1) + str(5))
         return set(moves)
@@ -47,5 +43,23 @@ class King(Figure):
                  if 1 <= self.point.pos_y + b <= 8]
         return set(moves)
 
+    def delete(self):
+        pass
+
+
+class Knight(Figure):
+    def __init__(self, color: int, point: Point):
+        self.color = color
+        self.moves = self.move()
+        self.point = point
+
+    def move(self) -> set:
+        moves = []
+        knight_moves = [[1, 2], [2, 1], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
+        moves += [str(self.point.pos_x + a) + str(self.point.pos_y + b) for a, b in knight_moves 
+                  if 1 <= self.point.pos_x + a <= 8
+                  if 1 <= self.point.pos_y + b <= 8]
+        return set(moves)
+    
     def delete(self):
         pass
